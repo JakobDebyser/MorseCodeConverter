a412591736c0a50f9e1f1d78f7a7a4a50bd55058
Jakob Debyser <jakob.debyser@hotmail.com>
2024-09-14 10:56:54 +0200

    program done

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-print("Hello")
+CODE = {
+    ' ':'_',
+    'A':'.-',
+    'B':'-...',
+    'C':'-.-.',
+    'D':'-..',
+    'E':'.',
+    'F':'..-.',
+    'G':'--.',
+    'H':'....',
+    'I':'..',
+    'J':'.---',
+    'K':'-.-',
+    'L':'.-..',
+    'M':'--',
+    'N':'-.',
+    'O':'---',
+    'P':'.--.',
+    'Q':'--.-',
+    'R':'.-.',
+    'S':'...',
+    'T':'-',
+    'U':'..-',
+    'V':'...-',
+    'W':'.--',
+    'X':'-..-',
+    'Y':'-.--',
+    'Z':'--..'
+}
+def convert_to_morse_code(sentence):
+    encodedsentence = ''
+    sentence = sentence.upper()
+    for char in sentence:
+        encodedsentence+=CODE[char]
+    return encodedsentence
+sentence = input('Enter sentence: ')
+encodedSentence=convert_to_morse_code(sentence)
+print(encodedSentence)
